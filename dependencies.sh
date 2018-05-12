17f65b61f5c9e8338d48b1faf3072185ff81b154
Sausy <Sausy@users.noreply.github.com>
2018-05-12 12:34:22 +0200

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,4 +1,4 @@
-sudo apt-get install python-pip python-dev python-setuptools python-virtualenv git libyaml-dev build-essential raspi-gpio xcompmgr dbus-x11 libgl1-mesa-dri xserver-xorg xserver-xorg-video-fbdev xserver-xorg-input-evdev xinit xfonts-100dpi xfonts-75dpi xfonts-cyrillic xfonts-scalable xinput ttf-mscorefonts-installer xinput-calibrator  
+sudo apt-get install python-pip python-dev python-setuptools python-virtualenv git libyaml-dev build-essential raspi-gpio xcompmgr dbus-x11 libgl1-mesa-dri xserver-xorg xserver-xorg-video-fbdev xserver-xorg-input-evdev xinit xfonts-100dpi xfonts-75dpi xfonts-cyrillic xfonts-scalable xinput ttf-mscorefonts-installer xinput-calibrator xterm   
 sudo apt-get install chromium-browser
 
 #optional if not only chromium shall be shown 
